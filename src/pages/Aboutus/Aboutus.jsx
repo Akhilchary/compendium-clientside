b269e08ead6d19d68588750822e3d1c719099943
ANKIT11RAO <ankit123goku@gmail.com>
2022-03-15 22:05:11 +0530

    Update Aboutus.jsx

--- src/pages/Aboutus/Aboutus.jsx
+++ src/pages/Aboutus/Aboutus.jsx
@@ -5,8 +5,9 @@ import "../Aboutus/Aboutus.css";
 import Logo from "../../images/Logo (1) 2.svg";
 import Founder from "../../images/IMG_6045 1.png";
 import Creativedirector from "../../images/IMG_5936 1.png"; 
-import President from "../../images/_DSC0251 1.png";
-import Managingdirector from "../../images/_DSC0341 1.png";
+import President from "../../images/_DSC0341 1.png";
+import Managingdirector from "../../images/jacob1.jpg";
+import cofounder from "../../images/cofounder1.png";
 // import Line from "../../images/Vector 25.png";
 // import Lines from "../../images/Group 71.svg";
 import linkedIcon from "../../images/linkdin-icon.svg";
@@ -29,8 +30,8 @@ function Aboutusbody(){
                 </div>
                 <h1 class="about">About</h1>
                 <img class="logo" src={Logo} alt="" />
-                <p className="about-text1">m ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et</p>
-                <p className="about-text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.sit amet, consectetur adipiscing elit. Aliquam dictum gravida et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum gravida et donec tellus donec pretium nunc.Lorem ipsum dolor sit amet,</p>
+                <p className="about-text1">The Compendium (TC) is the News and Publication Society (NPS) of the Institute of Aeronautical Engineering. The Compendium was started in 2019, making it one of the earliest student clubs on campus. </p>
+                <p className="about-text2">Awareness is all about restoring your freedom to choose what you want instead of what your past imposes on you. The Compendium provides an overall review and makes sure that everyone is informed about all that happens at IARE. From achievements of students and faculty, to events, workshops, and fests conducted; Departmental activities to providing a platform for exposure of talent for students; everything that happens in IARE, The Compendium covers it all for you. </p>
             </div>
 
             <div className="body-2">
@@ -42,7 +43,7 @@ function Aboutusbody(){
                     <p className="Foundertext">Founder</p>
                     <p className="name">Anusha Vajha</p>
                 </div>
-                <Link to="/cofounder"><img className="Cofounder" src={Founder} alt="" /></Link>
+                <Link to="/cofounder"><img className="Cofounder" src={cofounder} alt="" /></Link>
                 <div className="cofoundertextarea">
                     <p className="cofoundertext">Co-founder</p>
                     <p className="name">Anusha Vajha</p>
@@ -58,12 +59,12 @@ function Aboutusbody(){
                 <Link to="/president"><img className="president" src={President} alt="" /></Link>
                 <div className="presidenttextarea">
                     <p className="Presidenttext">President</p>
-                    <p className="name">Rahul Sattarapu</p>
+                    <p className="name">Sachin Pisipati</p>
                 </div>
                 <Link to="/managingdirector"><img className="Managingdirector" src={Managingdirector} alt="" /></Link>
                 <div className="managingdirectortextarea">
                     <p className="ManagingDirectortext">Managing Director</p>
-                    <p className="name">Sachin Pisipati</p>
+                    <p className="name">Jacob Alex</p>
                 </div>
             </div>
 
