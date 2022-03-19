42751fc8ad1a801c00bc739fbac07f303c53bf7d
ANKIT11RAO <ankit123goku@gmail.com>
2022-03-19 22:18:19 +0530

    Update managingdirector.jsx

--- src/pages/Aboutus/managingdirector.jsx
+++ src/pages/Aboutus/managingdirector.jsx
@@ -6,6 +6,8 @@ import Cross from "../../images/Vector 35.png";
 import linkedIcon from "../../images/linkdin-icon.svg";
 import instaIcon from "../../images/insta-icon.svg";
 import { Link } from "react-router-dom";
+import mailIcon from "../../images/mail.png";
+
 function Managingdirectorbody(){
     return(
         <div>
@@ -16,26 +18,29 @@ function Managingdirectorbody(){
             <p class="managingdirectorname">Managing Director</p>
             <p className="managingdirectordescription">Jacob Alex, a third year computer science engineering student who doesn't lack in compassion or spontaneity. He is tenacious, and wise. While spontaneity is a strong suit, he is also known to be thoughtful and calm. He's a good leader, as he makes sure that the people around him take their work seriously, and don't forget to have a life while they do it.<br />His strengths lie in identifying problems and the right people to solve them.</p>
         </div>
-        <div className="footer-3">
+        <div className="footer-AI">
 
-            <p className="soc-p1">Social.</p>
-            <img className="linkdin-ic1" src={linkedIcon} alt="" />
-            <img className="insta-ic1" src={instaIcon}  alt="" />
-
-            <div className="footer-nav1">
-                <h3>Navigate</h3>
-                <div className="footNav-content1">
-                    <p>About us</p>
-                    <p>Articles</p>
-                    <p>Gallery</p>
-                    <p>Spotlight</p>
-                    <p>Placements</p>
-                </div>
-            </div>
-
-            <p className="copyright">&copy;2021 TheCompendium  </p>
+                <p className="soc-p-art-H">Social.</p>
+                {/* <Link ><img className="linkdin-ic-H" src={linkedIcon} alt="" /></Link>
+                <Link><img className="insta-ic-H" src={instaIcon} alt=""  /></Link>
+                <Link><img className="mail-ic-H" src={mailIcon} alt=""  /></Link> */}
+                <a href="https://www.instagram.com/thecompendium.iare/" target='_blank' rel='noreferrer'><img className="linkdin-ic-H" src={linkedIcon} alt="" /></a>
+                <a href="https://www.linkedin.com/in/the-compendium-iare-987b35212/" target='_blank' rel='noreferrer'><img className="insta-ic-H" src={instaIcon} alt=""  /></a>
+                <Link><img className="mail-ic-H" src={mailIcon} alt=""  /></Link>
             
-        </div> 
+                <div className="footer-nav-H">
+                    <h3>Navigate</h3>
+                    <div className="footNav-content-H">
+                        <Link to={'/aboutus'}><p>About us</p></Link>
+                        <Link to={'articleslist'}><p>Articles</p></Link>
+                        <Link to={'/gallery'}><p>Gallery</p></Link>
+                        <Link to={'/gallery'}><p>Spotlight</p></Link>
+                        <Link to={'/placements'}><p>Placements</p></Link>
+    
+                    </div>
+                </div>
+                <p className="copyright-H">&copy;2021 TheCompendium  </p>
+            </div>
         </div>
     );
 }
