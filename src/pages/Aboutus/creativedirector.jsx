1ebbeaa4e098ba24c9cc3bf22f64fcf1c718a9c7
ANKIT11RAO <ankit123goku@gmail.com>
2022-04-05 18:32:21 +0530

    Update creativedirector.jsx

--- src/pages/Aboutus/creativedirector.jsx
+++ src/pages/Aboutus/creativedirector.jsx
@@ -34,7 +34,7 @@ function Creativedirectorbody(){
                         <Link to={'articleslist'}><p>Articles</p></Link>
                         <Link to={'/gallery'}><p>Gallery</p></Link>
                         <Link to={'/gallery'}><p>Spotlight</p></Link>
-                        <Link to={'/placements'}><p>Placements</p></Link>
+                        <Link to={'/archives'}><p>Archives</p></Link>
     
                     </div>
                 </div>
