0b6730c94f66c18d0a64add613a464c1afc00c6a
Akhilchary <akhilchary5418@gmail.com>
2022-04-05 14:09:16 +0530

    social links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer(){
             <p className="soc-p-art">Social.</p>
             <a href="https://www.linkedin.com/in/the-compendium-iare-987b35212/" target='_blank' rel='noreferrer'><img className="insta-ic" src={instaIcon} alt=""  /></a>
             <a href="https://www.instagram.com/thecompendium.iare/" target='_blank' rel='noreferrer'><img className="linkdin-ic" src={linkedIcon} alt="" /></a>
-            <img className="mail-ic" src={mailIcon} alt=""  />
+            <a href="mailto:iarethecompendium@gmail.com" ><img className="mail-ic" src={mailIcon} alt=""  /></a>
 
             <div className="footer-nav">
                 <h3>Navigate</h3>
