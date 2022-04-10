97a11af4b2ca24b744933ea1cf54c5c6f337b36a
ANKIT11RAO <ankit123goku@gmail.com>
2022-04-11 00:34:49 +0530

    Update Archives.jsx

--- src/pages/Archives/Archives.jsx
+++ src/pages/Archives/Archives.jsx
@@ -19,26 +19,50 @@ function Placementsbody(){
             <div className="Archives">
                 <p className="Archiveshead" >Newspaper Archives</p>
                 <p className="Archives-content">Our organization is run by IARE undergraduates. Up to _ students a year are involved in The Compendium, mastering skills such as reporting, journalism, writing, photography, digital production, videography, editing, graphic design and social media management while also learning effective communication and how to manage teams and their own schedules.</p>
-                <h1 className="number01">01</h1>
-                <p className="firstedition">First Edition</p>
-                <a href={issue1} target="__blank" > <img className="eye"  src={eye} alt="" /></a>
-                <Link download={issue1} target="__blank"> <img className="download" src={download} alt="" /></Link>
-                <h1 className="number02">02</h1>
-                <p className="secondedition">Second Edition</p>
-                <a href={issue2} target="__blank" > <img className="eye2"  src={eye} alt="" /></a>
-                <Link download={issue2} target="__blank"> <img className="download2" src={download} alt="" /></Link>
-                <h1 className="number03">03</h1>
-                <p className="thirdedition">Third Edition</p>
-                <a href={issue3} target="__blank" > <img className="eye3"  src={eye} alt="" /></a>
-                <Link download={issue3} target="__blank"> <img className="download3" src={download} alt="" /></Link>
-                <h1 className="number04">04</h1>
-                <p className="fourthedition">Fourth Edition</p>
-                <a href={issue4} target="__blank" > <img className="eye4"  src={eye} alt="" /></a>
-                <Link download={issue4} target="__blank"> <img className="download4" src={download} alt="" /></Link>
-                <h1 className="number05">05</h1>
-                <p className="fifthedition">Fifth Edition</p>
-                <a href={issue4} target="__blank" > <img className="eye5"  src={eye} alt="" /></a>
-                <Link download={issue4} target="__blank"> <img className="download5" src={download} alt="" /></Link>
+            </div>
+            <div class="timeline">
+                <div class="container left">
+                    <div class="content">
+                    <h2>AUGUST 2019</h2>
+                    <h1 className="number01">01</h1>
+                    <p className="firstedition">First Edition</p>
+                    <a href={issue1} target="__blank" > <img className="eye"  src={eye} alt="" /></a>
+                    <Link download={issue1} target="__blank"> <img className="download" src={download} alt="" /></Link>
+                    </div>
+                </div>
+                <div class="container right">
+                    <div class="content">
+                    <h2>SEPTEMBER 2019</h2>
+                    <h1 className="number02">02</h1>
+                    <p className="secondedition">Second Edition</p>
+                    <a href={issue2} target="__blank" > <img className="eye"  src={eye} alt="" /></a>
+                    <Link download={issue2} target="__blank"> <img className="download" src={download} alt="" /></Link></div>
+                </div>
+                <div class="container left">
+                    <div class="content">
+                    <h2>NOVEMBER 2019</h2>
+                    <h1 className="number03">03</h1>
+                    <p className="thirdedition">Third Edition</p>
+                    <a href={issue3} target="__blank" > <img className="eye"  src={eye} alt="" /></a>
+                    <Link download={issue3} target="__blank"> <img className="download" src={download} alt="" /></Link></div>
+                </div>
+                <div class="container right">
+                    <div class="content">
+                    <h2>JANUARY 2020</h2>
+                    <h1 className="number04">04</h1>
+                    <p className="fourthedition">Fourth Edition</p>
+                    <a href={issue4} target="__blank" > <img className="eye"  src={eye} alt="" /></a>
+                    <Link download={issue4} target="__blank"> <img className="download" src={download} alt="" /></Link></div>
+                </div>
+                <div class="container left">
+                    <div class="content">
+                    <h2>April 2022</h2>
+                    <h1 className="number05">05</h1>
+                    <p className="fifthedition">Fifth Edition</p>
+                    <a href={issue4} target="__blank" > <img className="eye"  src={eye} alt="" /></a>
+                    <Link download={issue4} target="__blank"> <img className="download" src={download} alt="" /></Link>
+                    </div>
+                </div>
             </div>
             <div className="footer-Ar">
                 <p className="soc-p-art-H">Social.</p>
